80ee4f55c00c4e20feef906d39e2ad2262149d61
robinmulamoottil <robinsimon.m@gmail.com>
2018-12-06 00:13:38 -0800

    initial

---
+++ R_Project06/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace R_Project06.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ R_Project06/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="R_Project06.Global" Language="C#" %>
